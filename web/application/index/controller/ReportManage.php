19dfc32886bbc9488a9d9dbd94a8c9a8aa025377
Summer <csy13808796047@gmail.com>
2020-11-24 16:27:16 +0800

    test

--- web/application/index/controller/ReportManage.php
+++ web/application/index/controller/ReportManage.php
@@ -128,7 +128,7 @@ class ReportManage extends Controller
         }
         //    dump($request->param());
         $params = $request->param();
-        if(!isset($params['isquery']) || !isset($params['id'])) {
+        if(!isset($params['isquery'])) {
 //            $this->assign('liqTypeName', $this->liqTypeName);
 //            $this->assign('modeConfig', $this->modeConfig);
 //            $this->assign('total', 0);
