b1a9d5af594b7255ca4968224162641aa38d521a
Summer <csy13808796047@gmail.com>
2020-11-26 10:19:26 +0800

    test

--- web/application/index/controller/ReportManage.php
+++ web/application/index/controller/ReportManage.php
@@ -144,8 +144,9 @@ class ReportManage extends Controller
         isset($params['id']) ? $user = Members::get($params['id']) : $user = session('userData');
 
         $builder = Members::where('parentId', $user['uid']);
-        if(!empty($para['user_name'])) {
-            $builder->where('username', $para['username']);
+        dump($params['username']);
+        if(!empty($params['username'])) {
+            $builder->where('username', $params['username']);
         }
         $childs = $builder->select();
         if(isset($params['days']) && isset($params['days2'])) {
