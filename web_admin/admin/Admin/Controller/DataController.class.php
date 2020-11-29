49701d9b9d3f877a796452a6be1d77236316b22b
Summer <darker_1987@qq.com>
2020-11-29 20:55:28 +0800

    'test'

--- web_admin/admin/Admin/Controller/DataController.class.php
+++ web_admin/admin/Admin/Controller/DataController.class.php
@@ -81,7 +81,7 @@ class DataController extends AdminController
                 //关联表（左链接  order表 as o 别名  on关系 t.xx = o.xxx          and  t.xxx  =  o.xxx  ）
 
 //                ->where(['b.data', ['exp', 'is not null']])                       // 条件
-                ->where(['type' => $type])
+                ->where(['o.type' => $type])
                 ->order('o.id desc')         // 排序
 
                 ->limit(20)  // 取几条
