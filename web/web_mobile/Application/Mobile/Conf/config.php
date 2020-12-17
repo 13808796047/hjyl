e4b7b01b43ccf8e580bd0a3911a2492b382fcbc3
Summer <csy13808796047@gmail.com>
2020-12-17 17:19:43 +0800

    PC-微信公众号登录

--- web/web_mobile/Application/Mobile/Conf/config.php
+++ web/web_mobile/Application/Mobile/Conf/config.php
@@ -74,7 +74,7 @@ return [
         '__IMG__' => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
         '__CSS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
         '__JS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
-        '__SCRIPT__' => __ROOT__ . '/Public/Mobile',
+        '__SCRIPT__' => '/Public/Mobile',
     ],
 
     /* SESSION 和 COOKIE 配置 */
