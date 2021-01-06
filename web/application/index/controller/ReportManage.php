03788357833fc5cc71c8c636fc299507f706c3af
初绍云 <summer@chushaoyundeiMac-Pro.local>
2021-01-06 23:10:30 +0800

    test

--- web/application/index/controller/ReportManage.php
+++ web/application/index/controller/ReportManage.php
@@ -591,7 +591,7 @@ class ReportManage extends Controller
         //        $pwhere['isDelete'] = 0;
         if (isset($para['username']) && $para['username'] != '') {
             $where['username'] = strtolower(trim($para['username']));
-            $uid = M('members')->field('uid')->where($where)->find()['uid'];
+            $uid = Db::table('gygy_members')->field('uid')->where($where)->find()['uid'];
         }
 
         // $childs = $builder->where('uid', session('userData.uid'))->select();
