7b3fd6870b85c6fc86574557870434d96de35fd6
Summer <csy13808796047@gmail.com>
2021-03-08 15:39:15 +0800

    test

--- web/application/route.php
+++ web/application/route.php
@@ -66,4 +66,4 @@ Route::get('gettxssc', 'index/Wufencai/gettxssc');
 // usdt
 Route::get('user/bank-store', 'index/User/userBankStore');
 Route::post('user/bank-store', 'index/User/userBankStore');
-Route::delete('users/bank-del', 'index/User/userBankDel');
+Route::delete('user/bank-del', 'index/User/userBankDel');
