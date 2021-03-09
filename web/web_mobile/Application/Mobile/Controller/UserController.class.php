afa82454eaa0fde1a7ccb25eb9b3f9d578023ee7
Summer <csy13808796047@gmail.com>
2021-03-09 15:42:54 +0800

    test

--- web/web_mobile/Application/Mobile/Controller/UserController.class.php
+++ web/web_mobile/Application/Mobile/Controller/UserController.class.php
@@ -390,7 +390,7 @@ class UserController extends HomeController
         $map = [];
         $map['uid'] = $this->user['uid'];
         $map['enable'] = 1;
-        $mybanks = D('MemberBank')->relation(true)->where($map)->where('bankId>0')->Select();
+        $mybanks = D('MemberBank')->where($map)->where('bankId>0')->Select();
         $this->assign('mybanks', $mybanks);
         $usdts = D('member_bank')->where($map)->where('bankId=0')->select();
         $this->assign('usdts', $usdts);
