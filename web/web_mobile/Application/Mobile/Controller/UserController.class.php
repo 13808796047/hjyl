2a14ec646a9e56ed91edd24ecc3b15a1d128f99a
Summer <csy13808796047@gmail.com>
2021-03-09 16:13:45 +0800

    test

--- web/web_mobile/Application/Mobile/Controller/UserController.class.php
+++ web/web_mobile/Application/Mobile/Controller/UserController.class.php
@@ -391,7 +391,8 @@ class UserController extends HomeController
         $map['uid'] = $this->user['uid'];
         $map['enable'] = 1;
         $mybanks = M('MemberBank')->alias('m')
-            ->join('left join bank_list AS o ON m.bankId=o.id')
+            ->join('left join bank_list b ON m.bankId=b.id')
+            ->field('m.*,b.name')
             ->where($map)->where('bankId>0')->select();
         $this->assign('mybanks', $mybanks);
         $usdts = D('member_bank')->where($map)->where('bankId=0')->select();
