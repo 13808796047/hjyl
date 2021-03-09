3bf3bc77cd07ce0efa53298b03839d5107483894
Summer <csy13808796047@gmail.com>
2021-03-09 15:47:41 +0800

    test

--- web/web_mobile/Application/Mobile/Model/MemberBankModel.class.php
+++ web/web_mobile/Application/Mobile/Model/MemberBankModel.class.php
@@ -1,11 +1,15 @@
 <?php
 namespace Home\Model;
+
 use Think\Model\RelationModel;
-class MemberBankModel extends RelationModel{
+
+class MemberBankModel extends RelationModel
+{
     protected $_link = array(
-        'bankList'=>array(
-            'mapping_type'      => self::BELONGS_TO,
-            'mapping_fields' =>'bankId',
-            ),
-        );
+        'bankList' => array(
+            'mapping_type' => 2,
+            'class_name' => 'bank',
+            'foreign_key' => 'bankId',
+        ),
+    );
 }
