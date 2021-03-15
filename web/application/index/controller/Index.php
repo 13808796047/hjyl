6dcc2e0abb11996de0d5496a123eb30f0c2fba66
Summer <csy13808796047@gmail.com>
2021-03-15 14:03:14 +0800

    test

--- web/application/index/controller/Index.php
+++ web/application/index/controller/Index.php
@@ -2,33 +2,37 @@
 namespace app\index\controller;
 
 use app\index\model\Content;
+use app\index\model\Params;
 use think\Controller;
 use think\Session;
 use think\View;
-use app\index\model\Params;
-class Index extends  Controller
+
+class Index extends Controller
 {
     public function index()
     {
-    	// 客服
-    	 $kefu =Params::getParams('kefuGG')['kefuGG'];
-    	  $stauts =Params::getParams('kefuStatus')['kefuStatus'];
-    
-		View::share(['kefu'=> $kefu,'status'=>$stauts]);
+        // 客服
+        $kefu = Params::getParams('kefuGG')['kefuGG'];
+        $stauts = Params::getParams('kefuStatus')['kefuStatus'];
+
+        View::share(['kefu' => $kefu, 'status' => $stauts]);
         $json = config('multilingual');
 
-        $this->assign('foot','footer');
+        $this->assign('foot', 'footer');
         $m_c = new Content();
-        $this->assign('play_lists',(new Game())->play_list);
+        $this->assign('play_lists', (new Game())->play_list);
         $notice = $m_c->getNotice();
-        $this->assign('hash',md5(Session::get('userData.sessionId')));
-        $this->assign('notice',$notice);
-        $this->assign('notice_first',count($notice)>0?$notice[0]:array());
-        return view('index/index');
+        $this->assign('hash', md5(Session::get('userData.sessionId')));
+        $this->assign('notice', $notice);
+        $this->assign('notice_first', count($notice) > 0 ? $notice[0] : array());
+        return view('index/index', [
+            'usdt_setting' => Params::getParams('usdt_setting')['usdt_setting'],
+
+        ]);
     }
 
 //    public function getLogin()
-//    {
-//        return view('index/login');
-//    }
+    //    {
+    //        return view('index/login');
+    //    }
 }
--- web/application/index/controller/Login.php
+++ web/application/index/controller/Login.php
@@ -8,11 +8,9 @@
 
 namespace app\index\controller;
 
-
 use app\index\model\Members;
 use think\Controller;
 use think\Request;
-use think\Response;
 use think\Session;
 use think\Validate;
 
@@ -29,21 +27,21 @@ class Login extends Controller
     {
         $rules = [
 //            'username|用户名'  => 'require|max:25',
-            'loginpass|密码'   => 'require',
-            'validcode|验证码'   => 'require|captcha',
+            'loginpass|密码' => 'require',
+            'validcode|验证码' => 'require|captcha',
         ];
         $validate = new Validate($rules);
         $data = $request->param();
-        $result   = $validate->check($data);
-        if(!$result){
+        $result = $validate->check($data);
+        if (!$result) {
             $this->error($validate->getError());
         }
         $map['username'] = $request->param('username');
-        $user             = Members::where($map)->find();
- 
-        if(!$user){
+        $user = Members::where($map)->find();
+
+        if (!$user) {
             $this->error('用户不存在！');
-        }else{
+        } else {
             if ($user['isDelete'] == 1) {
                 $error = '用户已被删除';
                 $this->error($error);
@@ -56,21 +54,21 @@ class Login extends Controller
                 $error = '用户不存在';
                 $this->error($error);
             }
-        
-            if(think_ucenter_md5($request->param('loginpass'), UC_AUTH_KEY) !== $user['password']){
+
+            if (think_ucenter_md5($request->param('loginpass'), UC_AUTH_KEY) !== $user['password']) {
                 $this->error('密码或用户名不正确！');
             }
 
             $session = array(
-                'uid'         => $user['uid'],
-                'username'    => $user['username'],
+                'uid' => $user['uid'],
+                'username' => $user['username'],
                 'session_key' => session_id(),
-                'loginTime'   => time(),
-                'accessTime'  => time(),
-                'loginIP'     => $request->ip(),
+                'loginTime' => time(),
+                'accessTime' => time(),
+                'loginIP' => $request->ip(),
             );
             $member_session = db('member_session');
-            $re =  $member_session->insert($session);
+            $re = $member_session->insert($session);
             if (!$re) {
                 $this->error('插入登陆记录表失败，登陆失败');
             }
@@ -78,29 +76,30 @@ class Login extends Controller
             $user['sessionId'] = $member_session->getLastInsID();
 
             $member_session->where('uid=' . $user['uid'] . ' and id<' . $user['sessionId'])
-                ->update(['isOnLine'=> 0]);
-            Session::set('isLogin',true);
-            Session::set('userData',$user);
+                ->update(['isOnLine' => 0]);
+            Session::set('isLogin', true);
+            Session::set('userData', $user);
             Session::set('user_auth_sign2', data_auth_sign($_SERVER['HTTP_USER_AGENT'])); //session实现ip认证，防止session被盗取时别人可以登录。在adminControll中验证ip是否一致
             //$this->success('登录成功！', url('index/index'),$user['uid']);
-            return json(["code"=>1,"msg"=>"登录成功！","data"=>$user['uid'],"url"=>url('index/index'),"wait"=>3]);
+
+            return json(["code" => 1, "msg" => "登录成功！", "data" => $user['uid'], "url" => url('index/index'), "wait" => 3]);
         }
     }
 
     public function getLogout()
     {
-    	$user = Session::get('userData');
-    	// var_dump(isset($user['uid']));die;
-    	if(!isset($user['uid'])){
-			 return $this->redirect('index/login');
-    	}
-        
-    		$where = isset($user['sessionId']) ? 'uid=' . $user['uid'] . ' and id=' . $user['sessionId'] : 'uid=' . $user['uid'];
-        	db('member_session')->where($where)->update(['isOnLine'=> 0]);
+        $user = Session::get('userData');
+        // var_dump(isset($user['uid']));die;
+        if (!isset($user['uid'])) {
+            return $this->redirect('index/login');
+        }
+
+        $where = isset($user['sessionId']) ? 'uid=' . $user['uid'] . ' and id=' . $user['sessionId'] : 'uid=' . $user['uid'];
+        db('member_session')->where($where)->update(['isOnLine' => 0]);
 
         Session::clear();
         Session::destroy();
-       
+
         return $this->redirect('index/login');
     }
 }
