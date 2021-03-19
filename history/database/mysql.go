b28c92208eed9b94f572740e2093ca121ca3b1e2
Summer <csy13808796047@gmail.com>
2021-03-19 17:21:45 +0800

    test

--- history/database/mysql.go
+++ history/database/mysql.go
@@ -9,12 +9,15 @@ var Eloquent *gorm.DB
 
 func init() {
 	Eloquent, _ = gorm.Open("mysql", "root:GUPMTMD3o1uSI3gq@tcp(127.0.0.1:3306)/hjyl?charset=utf8")
-
+	gorm.DefaultTableNameHandler = func(Eloquent *gorm.DB, defaultTableName string) string {
+		return "gygy_" + defaultTableName
+	}
+	CloseDB()
 	// if err != nil {
 	// 	fmt.Printf("mysql connect error %v", err)
 	// }
 }
 
-// func CloseDB() {
-// 	defer Eloquent.Close()
-// }
+func CloseDB() {
+	defer Eloquent.Close()
+}
