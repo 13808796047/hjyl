d067f73bfb97cabf2c6162a3be6b5e12f7cd3a8d
Summer <csy13808796047@gmail.com>
2021-03-19 17:49:39 +0800

    test

--- history/models/data.go
+++ history/models/data.go
@@ -13,6 +13,6 @@ type Data struct {
 }
 
 func GetDatas(maps map[string]interface{}) (datas []Data) {
-	orm.Eloquent.Where("type=?", 36).Limit(1).Find(&datas)
+	orm.Eloquent.Where("type=?", 36).Where("time > ? AND time < ?", maps["start_date"], maps["end_date"]).Find(&datas)
 	return
 }
